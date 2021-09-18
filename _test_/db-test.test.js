839f1fd9a1b39a68bbd78e9f528016717b8d87ff
Joseph-K-B <303josephb@gmail.com>
2021-09-17 19:14:01 -0700

    All four tests pass but thres something wrong with beforeEach. The rm is removing files everytime tests are ran

--- _test_/db-test.test.js
+++ _test_/db-test.test.js
@@ -1,48 +1,42 @@
-import { rm, mkdir } from 'fs/promises';
-import { SimpleDb } from '../store/simpleDb.js';
+// import { rm, mkdir } from 'fs/promises';
+import { SimpleDb } from '../simpleDb.js';
 
 describe('SimpleDb', () => {
-  const rootDir = '../store/simpleDb.js';
-
-  beforeEach(() => {
-    return rm(rootDir, { force: true, recursive: true }).then(() => {
-      return mkdir(rootDir, { recursive: true });
-    });
-  });
+  const store = './store';
+  
+  // beforeEach(() => {
+  //   return rm(store, { recursive: true, force: true }).then(() => {
+  //     return mkdir(store, { recursive: true });
+  //   });
+  // });
 
   it('it checks new object for id', () => {
-    const file = new SimpleDb(rootDir);
-    const Martin = {
-      instrument: 'guitar',
-      type: 'acoustic',
-    };
+    const file = new SimpleDb(store);
+    const Martin = { instrument: 'guitar' };
     
     return file
       .save(Martin)
       .then(() => {
         expect(Martin.id).toEqual(expect.any(String));
+        console.log(Martin.id);
       });
   });
 
   it('it saves new object and gets it from database', () => {
-    const file = new SimpleDb(rootDir);  
-    const Martin = {
-      instrument: 'guitar',
-      type: 'acoustic',
-      
-    };
+    const file = new SimpleDb(store);  
+    const Taylor = { instrument: 'guitar' };
     return file
-      .save(Martin)
+      .save(Taylor)
       .then(() => { 
-        return file.get(Martin.id);
+        return file.get(Taylor.id);
       })
       .then((newFile) => {
-        expect(newFile.id).toEqual(Martin.id);
+        expect(newFile.id).toEqual(Taylor.id);
       });
   });
   
   it('returns null when id isnt present', () => {
-    const Yamaha = new SimpleDb(rootDir);
+    const Yamaha = new SimpleDb(store);
 
     return Yamaha
       .get(867)
@@ -51,7 +45,21 @@ describe('SimpleDb', () => {
       });
   });
 
-  
+  it('returns objects from directory', () => {
+    const main = './store';
+    const files = new SimpleDb(main);
+    return files.getAll()
+      .then((files) => {
+        expect(files).toEqual([
+          { instrument: 'guitar', 
+            id: expect.any(String) 
+          },
+          { instrument: 'guitar', 
+            id: expect.any(String) 
+          }
+        ]);
+      });
+  });
 });
 
 
---
+++ simpleDb.js
@@ -0,0 +1,59 @@
+import { readFile, writeFile, readdir } from 'fs/promises';
+import path from 'path';
+import shortid from 'shortid';
+
+export class SimpleDb {
+  constructor(store){
+    this.store = store;
+    // const newFile = `${this.fileId}.json`;
+  }
+
+  getPath(id) {
+    return `${this.store}/${id}.json`;
+  }
+  
+  save(obj) {
+    obj.id = shortid.generate();
+    // obj.id = this.fileId;
+    // const data = `${obj.id}.json`;
+    // this.file = path.join(this.store, data);
+    return writeFile(this.getPath(obj.id), JSON.stringify(obj))
+      .then(() => {
+        return obj.id;
+      });
+  }
+
+  get(id) {
+    const name = `${id}.json`;
+    this.file = path.join(this.store, name);
+    return readFile(this.file, 'utf-8')
+      .then((res) => {
+        return JSON.parse(res);
+      }).catch((err) => {
+        if (err)  {return null;
+        }
+        throw err;
+      });
+  }
+
+  getAll() {
+    const src = './store';
+    return readdir(src)
+      .then((files) => {
+        return Promise.all(
+          files.map((file) => {
+            return path.join(src, file);
+          })
+        )
+          .then((target) => {
+            return Promise.all(
+              target.map(x => {
+                return readFile(x, 'utf-8')
+                  .then(parseFile => JSON.parse(parseFile));
+              })
+            );
+          });
+      });
+  }
+}
+
